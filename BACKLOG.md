4ac1c365b9eae424022572af8a00bcdc883eb5bb
Sy <simon.tsegay@gmail.com>
2019-02-23 13:06:26 -0500

    created a backlog with next steps

---
+++ BACKLOG.md
@@ -0,0 +1,3 @@
+# Back Log
+* Seed the data for projects on `<Bits />` instead of manual input.
+* Create transitions between components
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./Main";
-
-const mountNode = document.getElementById("root");
-
-ReactDOM.render(<Main />, mountNode);
