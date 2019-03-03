709ceb93b928eb44ddf7724631a2f1201756a587
Sy <simon.tsegay@gmail.com>
2019-03-03 17:31:07 -0500

    improved mobile responsiveness of navbar

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+
 import "./style.css";
+import "./responsive.css";
 
 export class NavBar extends Component {
 
