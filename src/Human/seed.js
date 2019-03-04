746717b63ae0854dd32c9925a4fcdadf403ef1b7
Sy <simon.tsegay@gmail.com>
2019-03-03 19:26:25 -0500

    moved about tabs into seed file, cleaned up <Human/> and 'responsivized' it

---
+++ src/Human/seed.js
@@ -0,0 +1,32 @@
+import uuid from 'uuid';
+
+export const about = [{
+  id: uuid.v4(),
+  label: "About",
+  description: "I'm a full-stack web developer based in New York City, experienced in building and deploying user-friendly, seamless web applications from wireframe to production. When not wearing the hat of a developer, I enjoy engaging the world as a photographer, creating and sharing visual memoirs that tell stories through my lens.",
+  hidden: false,
+}, {
+  id: uuid.v4(),
+  label: "Early Life",
+  description: "Growing up, my family and I lived in several countries throughout East Africa & the Middle East.I was a pre-teen when we migrated to the U.S. and settled in the San Francisco Bay Area - California.",
+  hidden: false,
+}, {
+  id: uuid.v4(),
+  label: "Adult Life",
+  description: "I naturally gravitated towards technology. I studied computer science in college and worked in I.T. for several years. I thought of it as a rite of passage living in the Silicon Valley for decades. I also volunteered in my community as a youth mentor and tutor at the Boys & Girls Club. I found it deeply rewarding to impact young people in a positive way, so I transitioned into full-time non-profit work focused around tech education and youth development throughout the South Bay Area. Meanwhile, I was actively working on the side towards turning my passion for photography into my dream job.",
+  hidden: false,
+}, {
+  id: uuid.v4(),
+  label: "Creativity",
+  description: "Creativity is a gift to me, and it fills me with joy to share its fruit. Since discovering photography as a teen, I appreciated how it continues to challenge me in expressing a unique way of seeing the world. It challenges me to perceive a kind of beauty in everything, every moment. I find photographs to have an incredible power in their ability to invoke deep, genuine emotional connections while allowing me to savor and relive a distant moment. Having run a digital creative media business, it’s been a tremendous honor to capture treasured moments for clients all over the world.",
+  hidden: false,
+}, {
+  id: uuid.v4(),
+  label: "Leisure",
+  description: "My fiance and I currently live in the Bed-Stuy neighborhood of Brooklyn. In our leisure time, we enjoy exploring the outdoors, traveling the world and immersing in all its cultures together. Whenever my schedule allows for some down time, I keep up with basketball and football. I enjoy theater, chess and ping-pong. I'm also an avid fan of sci-fi novels, films and TV! 🤓🖖",
+  hidden: false,
+}];
+
+export default {
+  about
+};
