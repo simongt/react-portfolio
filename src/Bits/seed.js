d1830433404915d567a948eaa5c668ff7f3b536b
Sy <simon.tsegay@gmail.com>
2019-03-12 16:02:56 -0400

    added new project GIF for Memor-Eyes

--- src/Bits/seed.js
+++ src/Bits/seed.js
@@ -77,7 +77,7 @@ export const projects = [{
   title: "MemorEyes (VanillaJS)",
   gifUrl: '/img/bits/memor-eyes.gif',
   imgUrl: '/img/bits/memor-eyes.jpg',
-  description: "Used HTML, CSS + Flexbox, Vanilla JS to build MemorEyes, a simple memory-matching game that lets users flip cards to find pairs with matching ranks.",
+  description: "Used HTML, CSS + Flexbox, Vanilla JS to build MemorEyes, a memory game that lets users flip cards to find pairs with matching ranks.",
   live: 'https://simongt.github.io/memor-eyes/',
   github: 'https://github.com/simongt/memor-eyes/',
   hidden: false,
